e0ab6940de3a857b8f6ae4d5f053528e71ceedb2
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-01-30 21:38:02 +0300

    Initial commit

--- src/main/java/lesson1/task3/BoxWithFruits.java
+++ src/main/java/lesson1/task3/BoxWithFruits.java
@@ -2,22 +2,50 @@ package lesson1.task3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 public class BoxWithFruits<T extends Fruit> {
-    private List<T> container;
-   public BoxWithFruits() {this.container = new ArrayList<>();}
-    public BoxWithFruits(T...fruits){
-       this.container = new ArrayList<>(Arrays.asList(fruits));
-   }
-    public float getWeight{
-       float w=0.0f;
-        for (T fruit : container) {
-          w += fruit.getWeight();
+
+
+    private ArrayList<T> list;
+
+    public BoxWithFruits() {
+        this.list = new ArrayList<>();
+    }
+
+
+    public BoxWithFruits(T... fruits) {
+        this.list = new ArrayList<>(Arrays.asList(fruits));
+    }
+
+    public float getWeight() {
+        float weight = 0.0f;
+
+        for (T o : list) {
+            weight += o.getWeight();
         }
-       return w;
+
+        return weight;
     }
-    public boolean sameAvg(BoxWithFruits<?> another){
-       return Math.abs(this.getWeight()-another.getWeight())< 0.001;
+
+    public void pour(BoxWithFruits<T> another) {
+        another.list.addAll(list);
+        list.clear();
     }
+
+    public void add(T fruit) {
+        list.add(fruit);
+    }
+
+    public void add(Collection<T> fruit) {
+        list.addAll(fruit);
+    }
+
+    public boolean compare(BoxWithFruits<?> o) {
+
+        // избегаем ошибки округления с помощью дельты
+        return Math.abs(this.getWeight() - o.getWeight()) < 0.001;
+    }
+
 }
--- src/main/java/lesson1/task3/Main.java
+++ src/main/java/lesson1/task3/Main.java
@@ -1,4 +1,46 @@
 package lesson1.task3;
 
 public class Main {
+  public static void main(String[] args) {
+    final BoxWithFruits<Apple> box1 = new BoxWithFruits<>();
+    box1.add(new Apple());
+    box1.add(new Apple());
+    box1.add(new Apple());
+    final float appleBoxWeight = box1.getWeight();
+    System.out.println("Вес коробки с яблоками = " + appleBoxWeight);
+
+    final BoxWithFruits<Orange> box2 = new BoxWithFruits<>();
+    box2.add(new Orange());
+    box2.add(new Orange());
+    box2.add(new Orange());
+    final float orangeBoxWeight = box2.getWeight();
+    System.out.println("Вес коробки с апельсинами =" + orangeBoxWeight);
+    System.out.println("Коробки равны по весу?  " + box1.compare(box2) + "\n");
+
+    System.out.println("Создаем новые коробки, c 3 апельсинами  и c  5 яблоками  пересыпаем в" +
+            " них 3 апельсина и 3 яблока из старых коробок соответственно");
+
+    final BoxWithFruits<Apple> box3 = new BoxWithFruits<>();
+    box3.add(new Apple());
+    box3.add(new Apple());
+    box3.add(new Apple());
+    box3.add(new Apple());
+    box3.add(new Apple());
+    box3.add(new Apple());
+    box1.pour(box3);
+    final double appleBoxWeightTwo = box3.getWeight();
+    System.out.println("Вес коробки с яблоками: " + appleBoxWeightTwo);
+
+    final BoxWithFruits<Orange> box4 = new BoxWithFruits<>();
+    box4.add(new Orange());
+    box4.add(new Orange());
+    box4.add(new Orange());
+    box2.pour(box4);
+    final double orangeBoxWeightTwo = box4.getWeight();
+
+
+    System.out.println("Вес коробки с апельсинами: " + orangeBoxWeightTwo);
+    System.out.println("Коробки равны по весу?  " + box3.compare(box4));
+
+  }
 }
